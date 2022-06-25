4b95bd8833a72730f1a3fc8fe7bca7a1dc10064f
yyh <74586856+yangyihui2020@users.noreply.github.com>
2022-06-25 21:03:14 +0800

    Add files via upload

---
+++ cpp/io/example.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main()
+{
+    std::cout <<"enter two number:"<<std::endl;
+    int v1,v2;
+    std::cin>>v1>>v2;
+    std::cout<<"the sum of "<<v1<<"and"<<v2<<" is "<<v1+v2<<std::endl;
+    system("pause");
+    return 0;
+}
